a2dfc3ba6e7519bb754ef3dcf8a772a8f17e4149
Chris Grimmett <chris@grimtech.net>
2015-12-05 08:46:41 -0800

    fix ternary operator misuse

--- one.js
+++ one.js
@@ -9,13 +9,13 @@ var text = fs.readFileSync(path.join(__dirname, '1.md'), {encoding: 'utf8'});
 
 
 var upOrDown = function upOrDown (direction) {
-    return floor += 1 ? direction == '(' : floor -= 1;
+    return direction == '(' ? floor += 1 : floor -= 1;
 }
 
 
 var floor = 0;
 _.forEach(text, upOrDown);
-console.log('all done. floor is ' + floor);
+console.log('Santa needs to go to floor ' + floor);
 
 
 	  
